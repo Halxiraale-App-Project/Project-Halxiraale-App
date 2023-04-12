63068eac51accaa0c234fc39c85901c0f18e2772
Abdiaziz Omar <abdiazizmohamedshire@gmail.com>
2023-04-12 21:58:47 +0300

    This changes was committed by Abdiaziz Omar (instructor helped us)

--- index.js
+++ index.js
@@ -16,53 +16,85 @@
 
 // **************************************************************
 
+const nextQuestion = document.querySelector("#nextQuestion");
+const timer = document.querySelector("#timer");
+timer.textContent = '00:00'
+
 // create a component function
-function queAndAnsw (que1, answe1, answe2, answe3, answe4) {
+function queAndAnsw (que1, choices) {
     const mainDiv = document.createElement("div")
     const queDiv = document.createElement("div")
     const quepan = document.createElement("span")
-    const answDiv = document.createElement("div")
-    const ans1p = document.createElement("p")
-    const ans2p = document.createElement("p")
-    const answ2Div = document.createElement("div")
-    const ans3p = document.createElement("p")
-    const ans4p = document.createElement("p")
+    const choicesElement = document.createElement("div");
 
     // connect elements
     mainDiv.appendChild(queDiv)
     mainDiv.appendChild(quepan)
-    mainDiv.appendChild(answDiv)
-    mainDiv.appendChild(ans1p)
-    mainDiv.appendChild(ans2p)
-    mainDiv.appendChild(answ2Div)
-    mainDiv.appendChild(ans3p)
-    mainDiv.appendChild(ans4p)
+
 
     // add content
     quepan.textContent = que1
-    ans1p.textContent = answe1
-    ans2p.textContent = answe2
-    ans3p.textContent = answe3
-    ans4p.textContent = answe4
+  
+    for (let i = 0; i < choices.length; i++) {
+        const choice = choices[i];
+    
+        const choiceElement = document.createElement("div");
+        choiceElement.classList.add("choice");
+        const selectedAnswers = JSON.parse(localStorage.getItem("selectedAnswers")) || [];
+        const currentQuestionIndex = 0;
+        // <input type="radio" id="choice-1" name="choices" value="choice-1">
+        const radioButton = document.createElement("input");
+        radioButton.setAttribute("type", "radio");
+        radioButton.setAttribute("id", `choice-${i}`);
+        radioButton.setAttribute("name", "choices");
+        radioButton.setAttribute("value", choice.choice);
+        radioButton.addEventListener("change", function () {
+         
+          selectedAnswers[currentQuestionIndex] = this.value;
+          localStorage.setItem("selectedAnswers", JSON.stringify(selectedAnswers));
+        });
+    
+        const label = document.createElement("label");
+        label.setAttribute("for", `choice-${i}`);
+        label.textContent = choice.choice;
+    
+        choiceElement.appendChild(radioButton);
+        choiceElement.appendChild(label);
+        choicesElement.appendChild(choiceElement);
+
+        mainDiv.appendChild(choicesElement)
+      }
+    
+    
 
     // add classes
     mainDiv.classList.add("prtdiv")
     queDiv.classList.add("que")
-    answDiv.classList.add("answ")
-    ans1p.classList.add("ans1")
-    ans2p.classList.add("ans2")
-    answ2Div.classList.add("answ2")
-    ans3p.classList.add("ans3")
-    ans4p.classList.add("ans4")
+
     
     // return the parent
     return mainDiv
 }
+
+
+
+// show next question
 
 // connect the entry point
 const quizEntry = document.querySelector(".que-entry")
 
 // call API
 axios.get("https://private-1b8698-gabischool.apiary-mock.com/questions")
-
 // response the data and check
+.then(response => {
+    const question = response.data[0].question
+    const choices = response.data[0].choices
+
+    nextQuestion.addEventListener("click", function showNextQuestion() {
+    
+        });
+
+
+    quizEntry.appendChild(queAndAnsw(question, choices))
+
+})

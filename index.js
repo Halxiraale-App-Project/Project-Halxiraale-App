f27d897c96742980697e9a3386bc2c016cf6575f
unknown <simplecarrier101@gmail.com>
2023-04-15 22:13:14 -0700

    added more code for the demo day

--- index.js
+++ index.js
@@ -17,26 +17,49 @@
 // **************************************************************
 
 
-const nextQuestion = document.querySelector(".next-question")
+
+
+
+
+const nextQuestion = document.querySelector("#nextQuestion");
+const timer = document.querySelector("#timer");
+let timeLimit = 60;
+let timeLeft = timeLimit;
+let timerId;
+timer.textContent = '01:00';
+
+// function to start the timer
+function startTimer() {
+  timerId = setInterval(() => {
+    timeLeft--;
+    timer.textContent = formatTime(timeLeft);
+    if (timeLeft === 0) {
+      clearInterval(timerId);
+      console.log("Time's up!");
+    }
+  }, 1000);
+}
+
+// function to format time
+function formatTime(time) {
+  const minutes = Math.floor(time / 60).toString().padStart(2, '0');
+  const seconds = (time % 60).toString().padStart(2, '0');
+  return `${minutes}:${seconds}`;
+}
 
 // create a component function 
-
-
-function queAndAnsw (que1, choices) {
+function queAndAnsw (que1, choices, questionNumber) {
     const mainDiv = document.createElement("div")
     const queDiv = document.createElement("div")
     const quepan = document.createElement("span")
     const choicesElement = document.createElement("div")
-
     
-
     // connect elements
     mainDiv.appendChild(queDiv)
     mainDiv.appendChild(quepan)
     
-
     // add content
-    quepan.textContent = que1
+    quepan.textContent = `Question ${questionNumber}: ${que1}`
 
     for (let i = 0; i < choices.length; i++) {
         const choice = choices[i];
@@ -46,18 +69,14 @@ function queAndAnsw (que1, choices) {
         const selectedAnswers = JSON.parse(localStorage.getItem("selectedAnswers")) || [];
         const currentQuestionIndex = 0;
 
-        // <input type="radio" id="choice-1" name="choices" value="choice-1">
-        
-
         const radioButton = document.createElement("input");
         radioButton.setAttribute("type", "radio");
         radioButton.setAttribute("id", `choice-${i}`);
         radioButton.setAttribute("name", "choices");
         radioButton.setAttribute("value", choice.choice);
         radioButton.addEventListener("change", function () {
-         
-          selectedAnswers[currentQuestionIndex] = this.value;
-          localStorage.setItem("selectedAnswers", JSON.stringify(selectedAnswers));
+            selectedAnswers[currentQuestionIndex] = this.value;
+            localStorage.setItem("selectedAnswers", JSON.stringify(selectedAnswers));
         });
     
         const label = document.createElement("label");
@@ -68,46 +87,44 @@ function queAndAnsw (que1, choices) {
         choiceElement.appendChild(label);
         choicesElement.appendChild(choiceElement);
         mainDiv.appendChild(choicesElement);
-
     }
     
-
     // add classes
     mainDiv.classList.add("prtdiv")
     queDiv.classList.add("que")
-    
     
     // return the parent
     return mainDiv
 }
 
-//   show next question 
-    ;
 // connect the entry point
 const quizEntry = document.querySelector(".que-entry")
 
 // call API
 axios.get("https://private-1b8698-gabischool.apiary-mock.com/questions")
-
     .then(response => {
-
+        let currentQuestionIndex = 0;
+        const selectedAnswers = JSON.parse(localStorage.getItem("selectedAnswers")) || [];
+        const question = response.data[currentQuestionIndex].question;
+        const choices = response.data[currentQuestionIndex].choices;
+        quizEntry.appendChild(queAndAnsw(question, choices, currentQuestionIndex + 1));
+        startTimer();
         
-        const question = response.data[0].question
-        const choices = response.data[0].choices
-
         nextQuestion.addEventListener("click", function showNextQuestion() {
-            console.log("nextQuestion");
+            clearInterval(timerId);
+            timeLeft = timeLimit;
+            currentQuestionIndex++;
+            if (currentQuestionIndex < response.data.length) {
+                const question = response.data[currentQuestionIndex].question;
+                const choices = response.data[currentQuestionIndex].choices;
+                quizEntry.textContent = "";
+                quizEntry.appendChild(queAndAnsw(question, choices, currentQuestionIndex + 1));
+                startTimer();
+                console.log(selectedAnswers);
+            } else {
+                console.log("Quiz completed!");
+            }
+
+        
+           });
         });
-        quizEntry.appendChild(queAndAnsw(question, choices))
-    })
-
-    
-        
-        
-    
-
-
-
-
-  
-  
